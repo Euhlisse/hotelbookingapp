e5b91af8fa753ee810a8e5f0677a066c5672fc17
Euhlisse <elise.morana@efrei.net>
2024-11-13 14:46:33 +0100

    Employee and Customer classes : attributes, constructors, getters and ToString

--- src/main/java/fr/efrei/domain/Customer.java
+++ src/main/java/fr/efrei/domain/Customer.java
@@ -1,4 +1,41 @@
 package fr.efrei.domain;
 
+import java.time.LocalDate;
+
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private int idNumber;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+
+    public Customer() { }
+    public Customer(String firstName, String lastName, int idNumber, String email, String phone, LocalDate birthDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getIdNumber() { return idNumber; }
+    public String getEmail() { return email; }
+    public String getPhone() { return phone; }
+    public LocalDate getBirthDate() { return birthDate; }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", idNumber=" + idNumber +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
 }
--- src/main/java/fr/efrei/domain/Employee.java
+++ src/main/java/fr/efrei/domain/Employee.java
@@ -1,4 +1,27 @@
 package fr.efrei.domain;
 
 public class Employee {
+    private String firstName;
+    private String lastName;
+    private String jobType;
+
+    public Employee() { }
+    public Employee(String firstName, String lastName, String jobType) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.jobType = jobType;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getJobType() { return jobType; }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", jobType='" + jobType + '\'' +
+                '}';
+    }
 }
